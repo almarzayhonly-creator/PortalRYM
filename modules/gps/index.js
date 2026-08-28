fbf5cf806877b74ebf0dcc1e12a38fecfbdefea3
almarzayhonly-creator <almarzayhonly@gmail.com>
2026-08-28 18:45:23 -0500

    V172 clean: connect GPS module to canonical app

--- modules/gps/index.js
+++ modules/gps/index.js
@@ -1,2 +1,12 @@
-/* Portal RYM V171 - GPS module boundary */
-(function(w,d){'use strict';if(!w.RYM_MODULES)return;w.RYM_MODULES.register('gps',{open:function(){d.body.dataset.rymModule='gps';if(typeof w.v113OpenGps!=='function')throw new Error('GPS canonical entrypoint unavailable');return w.v113OpenGps();}})})(window,document);
+/* Portal RYM V172 clean - GPS module boundary */
+(function(w,d){
+  'use strict';
+  if(!w.RYM_MODULES)return;
+  w.RYM_MODULES.register('gps',{
+    open:function(ctx={}){
+      d.body.dataset.rymModule='gps';
+      if(!w.RYM_GPS_APP||typeof w.RYM_GPS_APP.open!=='function')throw new Error('GPS app unavailable');
+      return w.RYM_GPS_APP.open(ctx);
+    }
+  });
+})(window,document);
