08b8ad2e2cd53b6744f13f445fd18d98f272b326
almarzayhonly-creator <almarzayhonly@gmail.com>
2026-08-28 12:42:40 -0500

    V171: add Revisados module boundary

---
+++ modules/revisados/index.js
@@ -0,0 +1,2 @@
+/* Portal RYM V171 - Revisados module boundary */
+(function(w){'use strict';if(!w.RYM_MODULES)return;w.RYM_MODULES.register('revisados',{open:function(){if(typeof w.v60OpenRevisados!=='function')throw new Error('Revisados canonical entrypoint unavailable');return w.v60OpenRevisados();}})})(window);
