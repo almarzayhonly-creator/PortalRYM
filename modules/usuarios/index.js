4702335314ce25b00cc346c84d90bdc02f1d5c31
almarzayhonly-creator <almarzayhonly@gmail.com>
2026-08-28 12:42:57 -0500

    V171: add Usuarios module boundary

---
+++ modules/usuarios/index.js
@@ -0,0 +1,2 @@
+/* Portal RYM V171 - Usuarios module boundary */
+(function(w){'use strict';if(!w.RYM_MODULES)return;w.RYM_MODULES.register('usuarios',{open:function(){if(typeof w.v70OpenUsers!=='function')throw new Error('Usuarios canonical entrypoint unavailable');return w.v70OpenUsers();}})})(window);
