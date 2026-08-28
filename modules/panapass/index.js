ff655f6d7d688b0cbea16e7ddf21d836e1089512
almarzayhonly-creator <almarzayhonly@gmail.com>
2026-08-28 12:49:40 -0500

    V171: scope Panapass module styles

--- modules/panapass/index.js
+++ modules/panapass/index.js
@@ -1,2 +1,2 @@
 /* Portal RYM V171 - Panapass module boundary */
-(function(w){'use strict';if(!w.RYM_MODULES)return;w.RYM_MODULES.register('panapass',{open:function(){if(typeof w.v70OpenPanapass!=='function')throw new Error('Panapass canonical entrypoint unavailable');return w.v70OpenPanapass();}})})(window);
+(function(w,d){'use strict';if(!w.RYM_MODULES)return;w.RYM_MODULES.register('panapass',{open:function(){d.body.dataset.rymModule='panapass';if(typeof w.v70OpenPanapass!=='function')throw new Error('Panapass canonical entrypoint unavailable');return w.v70OpenPanapass();}})})(window,document);
