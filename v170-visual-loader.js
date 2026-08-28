2698939f4ccdac17a33f760b1b818c09ed99b143
almarzayhonly-creator <almarzayhonly@gmail.com>
2026-08-27 21:28:49 -0500

    Load visual polish without touching application logic

---
+++ v170-visual-loader.js
@@ -0,0 +1,7 @@
+/* Portal RYM V170 visual-only loader */
+(function(){
+ if(document.getElementById('rym-v170-visual-polish'))return;
+ var l=document.createElement('link');
+ l.id='rym-v170-visual-polish';l.rel='stylesheet';l.href='/v170-visual-polish.css?v=170';
+ document.head.appendChild(l);
+})();
